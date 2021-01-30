6cb2cbe7c799c68df91530c78e5f3c016e1c88eb
tutu <476167635@qq.com>
2021-01-30 10:42:15 +0800

    xioazhang

---
+++ src/main/java/com/example/demo/xiaozhang.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class xiaozhang {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("fenzhi de chaungjian he ceshi ");
+
+    }
+
+}
